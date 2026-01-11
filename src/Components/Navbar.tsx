60df0228a2caced32e6c0cd129732b4bdb06bf2b
asadishaq15 <asadishaq392@gmail.com>
2026-01-12 02:42:23 +0500

    update

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -4,25 +4,19 @@ import Logo from '../assets/Logo.png';
 
 const Navbar: React.FC = () => {
   return (
-<nav className="bg-white shadow-md h-16 flex items-center">
-
-<div className="container mx-auto px-4">
-        <div className="flex items-center justify-between">
-          <div className="flex items-center">
-          <img
+<nav className="bg-white shadow-md h-20 flex items-center">
+  <div className="container mx-auto px-4">
+    <div className="flex items-center justify-between">
+    <img
   src={Logo}
   alt="Citizens Disability Logo"
-  className="h-full max-h-[62px] w-auto object-contain"
-/>
+  className="h-24 w-auto"
+ />
 
+    </div>
+  </div>
+</nav>
 
-
-
-          </div>
-        
-        </div>
-      </div>
-    </nav>
   );
 };
 
